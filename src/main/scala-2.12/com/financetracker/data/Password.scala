3ada6c562a9515b439442a9c1a112265fd2cc01f
Alexey Karasev <karasev.alexey@gmail.com>
2017-05-30 10:33:13 +0500

    added md5 to password

--- src/main/scala-2.12/com/financetracker/data/Password.scala
+++ src/main/scala-2.12/com/financetracker/data/Password.scala
@@ -2,13 +2,23 @@ package com.financetracker.data
 
 import doobie.imports.Meta
 import io.circe.Encoder
+import java.security.MessageDigest
+import java.util.Base64
 
-case class Password(value: String) extends AnyVal
+case class Password private (val value: String) extends AnyVal
 
 object Password {
+  // Don't expose password to API (each json will have password filtered out)
   implicit val encodePassword: Encoder[Password] =
-    Encoder.encodeString.contramap[Password](_.value)
+    Encoder.encodeString.contramap[Password](_ => "[filtered]")
 
   implicit val passwordMeta: Meta[Password] =
-    Meta[String].xmap[Password](x => Password(x), _.value)
+    Meta[String].xmap[Password](x => new Password(x), _.value)
+
+  val md5Encoder = MessageDigest.getInstance("MD5")
+  val base64Encoder = Base64.getEncoder
+
+  def apply(value: String): Password = {
+    new Password(base64Encoder.encodeToString(md5Encoder.digest(value.getBytes)))
+  }
 }
