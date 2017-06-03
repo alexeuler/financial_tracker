4f9d773ac82d3dafacd23b4c639fa8e06749a2c3
Alexey Karasev <karasev.alexey@gmail.com>
2017-06-03 20:20:44 +0500

    added patch to expenses

--- src/main/scala-2.12/com/financetracker/api/v1/ExpensesEndpoint.scala
+++ src/main/scala-2.12/com/financetracker/api/v1/ExpensesEndpoint.scala
@@ -8,6 +8,8 @@ import io.circe.syntax._
 import fs2.Task
 import fs2.interop.cats._
 import scala.language.implicitConversions
+import java.sql.Timestamp
+import shapeless._
 
 import com.financetracker.api._
 import com.financetracker.types._
@@ -41,6 +43,13 @@ object ExpensesEndpoint {
           )
         } yield expense.asJson
 
+      case req @ PATCH -> Root / IntVar(userId) / "expenses" / IntVar(expenseId) =>
+        for {
+          session <- sessionService.getSessionData(req)
+          form <- TaskAttempt.liftT(req.as[Json].map(UpdateExpenseForm(_)))
+          expense <- expenseService.update(ExpenseId(expenseId), form, session)
+        } yield expense.asJson
+
       case req @ DELETE -> Root / IntVar(userId) / "expenses" / IntVar(expenseId) =>
         for {
           session <- sessionService.getSessionData(req)
@@ -60,4 +69,17 @@ object ExpenseForm {
   import io.circe.generic.semiauto._
   implicit val encoder: Encoder[ExpenseForm] = deriveEncoder
   implicit val decoder: Decoder[ExpenseForm] = deriveDecoder
+}
+
+object UpdateExpenseForm {
+  def apply(json: Json): HList = {
+    var res: HList = HNil
+    val cursor = json.hcursor
+    cursor.downField("amount").as[Long].map(x => res = Amount(x) :: res)
+    cursor.downField("description").as[String].map(x => res = Description(x) :: res)
+    cursor.downField("comment").as[String].map(x => res = Comment(x) :: res)
+    cursor.downField("occuredAt").as[Timestamp].map(x => res = OccuredAt(x) :: res)
+    cursor.downField("userId").as[Int].map(x => res = UserId(x) :: res)
+    res
+  }
 }
