ca20e7126e5cab257850492c7284eb10b6385f55
Alexey Karasev <karasev.alexey@gmail.com>
2017-06-04 17:05:52 +0500

    added signup component

---
+++ frontend/src/components/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Signup = props => (
+  <div>Signup</div>
+);
+
+Signup.propTypes = {
+
+}
+
+export default Signup;
--- frontend/src/setup/Router.js
+++ frontend/src/setup/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from '../components/Login';
+import Signup from '../components/Signup';
 
-const Router = props => (
+const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
     </Switch>
   </BrowserRouter>
 );
