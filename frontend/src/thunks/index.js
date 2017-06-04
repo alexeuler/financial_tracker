8a60fab3b22d51793cba169fb24a3217fef1d296
Alexey Karasev <karasev.alexey@gmail.com>
2017-06-04 16:28:20 +0500

    added error handling

--- frontend/src/thunks/index.js
+++ frontend/src/thunks/index.js
@@ -1,6 +1,8 @@
 import * as api from '../api';
 import { actions as reduxActions } from '../reducers';
 
+const SERVER_FAILURE_MESSAGE = 'Could not connect to server. Please try again later.';
+
 export const login = payload =>
   async function loginThunk(dispatch) {
     dispatch(reduxActions.setLoadingLoginForm(true));
@@ -8,9 +10,21 @@ export const login = payload =>
     try {
       response = await api.login(payload);
     } catch (e) {
-      return dispatch(reduxActions.setErrorsLoginForm({ general: 'Could not connect to server' }));
+      return dispatch(reduxActions.setErrorsLoginForm({ general: SERVER_FAILURE_MESSAGE }));
     } finally {
       dispatch(reduxActions.setLoadingLoginForm(false));
+    }
+    if (response.error) {
+      switch (response.error.code) {
+        case 300:
+          return dispatch(reduxActions.setErrorsLoginForm({
+            general: 'Invalid email and/or password.',
+          }));
+        default:
+          return dispatch(reduxActions.setErrorsLoginForm({
+            general: response.error.message,
+          }));
+      }
     }
     return dispatch(reduxActions.setTokenSession(response.result));
   };
