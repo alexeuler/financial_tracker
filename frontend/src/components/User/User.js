2025e9c1fc2df7cb1de7181b1f7ad59a5d38dc18
Alexey Karasev <karasev.alexey@gmail.com>
2017-06-07 00:00:24 +0500

    finished user CRUD

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -4,13 +4,10 @@ import PropTypes from 'prop-types';
 const User = props => (
   <div className={`flex flex-row-l flex-column ${props.edit ? 'bg-light-gray' : ''}`}>
     <div className="flex flex-row">
-      <div className="pa2 w4">
+      <div className="pa2 w5 flex items-center">
         <div>{props.identity}</div>
       </div>
-      <div className="pa2 w5 flex flex-column justify-center">
-        <div className="b">{props.password}</div>
-      </div>
-      <div className="pa2 w3 flex items-center">
+      <div className="pa2 w4 flex items-center">
         {props.role}
       </div>
     </div>
