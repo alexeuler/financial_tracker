9f75671ab9ba2f9cac2243433fbff023fa64ba47
Alexey Karasev <karasev.alexey@gmail.com>
2017-06-12 09:02:55 +0500

    finished list spec

--- src/it/scala-2.12/com/financetracker/api/expenses/ListSpec.scala
+++ src/it/scala-2.12/com/financetracker/api/expenses/ListSpec.scala
@@ -19,7 +19,6 @@ class ListSpec extends FunSpec with Matchers with BeforeAndAfter with PropertyCh
   describe("GET /users/:id/expenses") {
     describe("admin") {
       it("returns list of all expenses for any user") {
-
         val expected: Json = json"""
           {
             "error" : null,
@@ -66,6 +65,116 @@ class ListSpec extends FunSpec with Matchers with BeforeAndAfter with PropertyCh
         }
       }
     }
+
+    describe("manager") {
+      it("fails with 300 unauthorized for any user expept himself") {
+        val expected: Json = json"""
+          {
+            "error" : {
+              "code": 300,
+              "message": "Unauthorized"
+            },
+            "result" : null
+          }      
+        """
+
+        async {
+          for {
+            userWithTokenAndUsers <- loggedInUser(Role.Manager)
+            user = userWithTokenAndUsers._3.find(_.role == Role.User).get
+            req = Request(
+              method = GET, 
+              uri = baseUrl / "users" / user.id.value.toString / "expenses", 
+              headers = authHeader(userWithTokenAndUsers._2)
+            )
+            response <- TaskAttempt.liftT(httpClient.fetchAs[Json](req))
+          } yield {
+            withClue(s"Response: $response, expected: $expected: ") {
+              compareJsonsIgnoring(List("id", "createdAt", "updatedAt", "userId"))(response, expected) shouldBe true
+            }
+          }
+        }
+      }
+    }
+
+    describe("user") {
+      it("fails with 300 unauthorized for any user expept himself") {
+        val expected: Json = json"""
+          {
+            "error" : {
+              "code": 300,
+              "message": "Unauthorized"
+            },
+            "result" : null
+          }      
+        """
+
+        async {
+          for {
+            userWithTokenAndUsers <- loggedInUser(Role.User)
+            user = userWithTokenAndUsers._3.find(_.role == Role.Admin).get
+            req = Request(
+              method = GET, 
+              uri = baseUrl / "users" / user.id.value.toString / "expenses", 
+              headers = authHeader(userWithTokenAndUsers._2)
+            )
+            response <- TaskAttempt.liftT(httpClient.fetchAs[Json](req))
+          } yield {
+            withClue(s"Response: $response, expected: $expected: ") {
+              compareJsonsIgnoring(List("id", "createdAt", "updatedAt", "userId"))(response, expected) shouldBe true
+            }
+          }
+        }
+      }
+
+      it("returns list of expenses for himself") {
+        val expected: Json = json"""
+          {
+            "error" : null,
+            "result" : [
+              {
+                "id" : "ignored",
+                "amount" : 100,
+                "description" : "User McDonalds",
+                "comment" : "Big Mac",
+                "occuredAt" : "1970-01-01 05:00",
+                "userId" : "ignored",
+                "createdAt" : "ignored",
+                "updatedAt" : "ignored"
+              },
+              {
+                "id" : "ignored",
+                "amount" : 200,
+                "description" : "User Shoes",
+                "comment" : null,
+                "occuredAt" : "1970-01-01 05:00",
+                "userId" : "ignored",
+                "createdAt" : "ignored",
+                "updatedAt" : "ignored"
+              }
+            ]
+          }      
+        """
+
+        async {
+          for {
+            userWithTokenAndUsers <- loggedInUser(Role.User)
+            user = userWithTokenAndUsers._3.find(_.role == Role.User).get
+            req = Request(
+              method = GET, 
+              uri = baseUrl / "users" / user.id.value.toString / "expenses", 
+              headers = authHeader(userWithTokenAndUsers._2)
+            )
+            response <- TaskAttempt.liftT(httpClient.fetchAs[Json](req))
+          } yield {
+            withClue(s"Response: $response, expected: $expected: ") {
+              compareJsonsIgnoring(List("id", "createdAt", "updatedAt", "userId"))(response, expected) shouldBe true
+            }
+          }
+        }
+      }
+    }
+
 
 
 //     describe("manager") {
